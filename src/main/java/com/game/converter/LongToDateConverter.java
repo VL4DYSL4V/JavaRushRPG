8fee6247492998aa58a04e2afe2c16a0fef122d7
VladislavArzamastsev <68342736+VladislavArzamastsev@users.noreply.github.com>
2021-05-07 12:54:08 +0300

    Date conversion fix #1

--- src/main/java/com/game/converter/LongToDateConverter.java
+++ src/main/java/com/game/converter/LongToDateConverter.java
@@ -2,11 +2,16 @@ package com.game.converter;
 
 import org.springframework.core.convert.converter.Converter;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 public final class LongToDateConverter implements Converter<Long, Date> {
     @Override
     public Date convert(Long source) {
-        return new Date(source);
+        Instant instant = Instant.ofEpochMilli(source);
+        ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(instant, ZoneId.of("UTC"));
+        return Date.from(zonedDateTime.toInstant());
     }
 }
