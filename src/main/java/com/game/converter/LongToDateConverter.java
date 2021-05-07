905b5a348cf82b208fb164513a264470f5e6f748
VladislavArzamastsev <68342736+VladislavArzamastsev@users.noreply.github.com>
2021-05-07 13:05:48 +0300

    Date conversion fix #2: use LocalDateTime

--- src/main/java/com/game/converter/LongToDateConverter.java
+++ src/main/java/com/game/converter/LongToDateConverter.java
@@ -2,16 +2,19 @@ package com.game.converter;
 
 import org.springframework.core.convert.converter.Converter;
 
-import java.time.Instant;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.Date;
 
 public final class LongToDateConverter implements Converter<Long, Date> {
     @Override
     public Date convert(Long source) {
-        Instant instant = Instant.ofEpochMilli(source);
-        ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(instant, ZoneId.systemDefault());
-        return Date.from(zonedDateTime.toInstant());
+//        Instant instant = Instant.ofEpochMilli(source);
+//        ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(instant, ZoneId.systemDefault());
+//        return Date.from(zonedDateTime.toInstant());
+        LocalDateTime date = new Timestamp(source).toLocalDateTime();
+
+        return Date.from(date.atZone(ZoneId.systemDefault()).toInstant());
     }
 }
